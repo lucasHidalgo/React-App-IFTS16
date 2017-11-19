1b53c1bc23b6fbd2be19cb13c3c882a2589fa80a
Nata Corteggiano <33436587+NatanaelCorteggiano@users.noreply.github.com>
2017-11-19 17:19:12 -0300

    App Bar
    Se agrega App Bar como Header, con un menú desplegable + instalacion de plugin React

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class Home extends Component{    
     constructor(){
        super();
-       this.state = {items:[]}     
-   }
+       this.state = {items:[]}
+            drawerOpened: false     
+   };
+   _toggleDrawer(){
+      this.setState({
+          drawerOpened: !this.state.drawerOpened
+     });
+   };
+
    componentWillMount(){  
     var _this = this;
     axios
@@ -17,15 +26,18 @@ export default class Home extends Component{
             items: result.data.results
           });
     });    
-   }   
+   };   
     render(){            
         return(
-            <div>
             <MuiThemeProvider>
-            <AppBar title="My AppBar" />            
-          </MuiThemeProvider>
+            <div>
+            
+            <AppBar title="Galería de Fotos" onLeftIconButtonTouchTap= {() => this._toggleDrawer()} />
+            <Drawer open={this.state.drawerOpened} docked={false} onRequestChange={() => this._toggleDrawer()} />            
+          
           <h4>esto es home</h4>
           </div>
+          </MuiThemeProvider>
         )     
     }   
 }
