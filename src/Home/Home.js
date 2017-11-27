a623d6718ca8f73db9f234d1d5dd404b7f56b145
Nata Corteggiano <33436587+NatanaelCorteggiano@users.noreply.github.com>
2017-11-26 22:51:47 -0300

    Modificaciones
    Ultimas modificaciones

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {redA700, grey800} from 'material-ui/styles/colors';
-
+import Album from '../Album/Album.js';
+import {BrowserRouter as Router, Route , Link} from 'react-router-dom';
+import {List, ListItem} from 'material-ui/List';
+import Posts from '../Posts/Posts.js';
+import Photos from '../Photos/Photos.js';
+import GaleriaFotos from '../GaleriaFotos/GaleriaFotos.js';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -14,15 +21,33 @@ const muiTheme = getMuiTheme({
 }); 
 
 export default class Home extends Component{    
-  
+    constructor(){
+       super();
+       this.state = {items:[]}
+            drawerOpened: false     
+   };
+   _toggleDrawer(){
+      this.setState({
+          drawerOpened: !this.state.drawerOpened
+     });
+   };
 
- 
+   componentWillMount(){  
+    var _this = this;
+    axios
+    .get("https://swapi.co/api/people/?fformat=json")
+    .then(function(result){
+        _this.setState({
+            items: result.data.results
+          });
+    });    
+   };   
     render(){            
         return(
             <MuiThemeProvider muiTheme={muiTheme}>
             <div>
               <h1>Bienvenidos a la Galería de Fotos Corteggiano Hidalgo</h1>
-          <h3>En esta página podremos ver una serie de albums con las fotos que contiene cada uno de ellos.<br/> Podremos acceder a los albums en el menú deplegable que aparece en la Galería de Albums
+          <h3>En esta página podremos ver una serie de albums con las fotos que contiene cada uno de ellos.<br/> Podremos acceder a los albums en el menú deplegable que aparece arriba a la izquierda
           de la página, ahí dentro podremos ver los albums y seleccionando el boton "Ver Fotos" podremos
           acceder al contenido de cada uno de ellos.<br/>
           <br/>
